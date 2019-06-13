a2c9b4ce7355cd3342c36679759ca112abb3395d
Andrew Wen <Wen.Andrew@mayo.edu>
2019-06-13 16:17:51 -0500

    Expansions on API implementations

--- src/main/java/edu/mayo/dhs/ievaluate/core/IEvaluateCore.java
+++ src/main/java/edu/mayo/dhs/ievaluate/core/IEvaluateCore.java
@@ -1,6 +1,44 @@
 package edu.mayo.dhs.ievaluate.core;
 
 import edu.mayo.dhs.ievaluate.api.IEvaluate;
+import edu.mayo.dhs.ievaluate.api.models.applications.ProfiledApplication;
+import edu.mayo.dhs.ievaluate.api.util.StorageProvider;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.UUID;
 
 public class IEvaluateCore implements IEvaluate {
+
+    public IEvaluateCore(File workingDir) {
+        init();
+    }
+
+    private void init() {
+
+    }
+
+    @Override
+    public Collection<ProfiledApplication> getRegisteredApplications() {
+        return null;
+    }
+
+    @Override
+    public ProfiledApplication getApplication(UUID uid) {
+        return null;
+    }
+
+    @Override
+    public void registerApplication(ProfiledApplication application) {
+
+    }
+
+    @Override
+    public StorageProvider getStorage() {
+        return null;
+    }
+
+    public static void main(String... args) {
+
+    }
 }

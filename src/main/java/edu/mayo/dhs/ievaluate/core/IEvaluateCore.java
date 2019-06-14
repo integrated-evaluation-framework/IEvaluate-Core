1af4fa552aeffadd72498d7e8dea7e6d491d33c7
Andrew Wen <Wen.Andrew@mayo.edu>
2019-06-14 17:39:05 -0500

    API Expansion, try to fetch other repository first as a submodule

--- src/main/java/edu/mayo/dhs/ievaluate/core/IEvaluateCore.java
+++ src/main/java/edu/mayo/dhs/ievaluate/core/IEvaluateCore.java
@@ -2,10 +2,12 @@ package edu.mayo.dhs.ievaluate.core;
 
 import edu.mayo.dhs.ievaluate.api.IEvaluate;
 import edu.mayo.dhs.ievaluate.api.models.applications.ProfiledApplication;
+import edu.mayo.dhs.ievaluate.api.plugins.IEvaluatePlugin;
 import edu.mayo.dhs.ievaluate.api.util.StorageProvider;
 
 import java.io.File;
 import java.util.Collection;
+import java.util.Map;
 import java.util.UUID;
 
 public class IEvaluateCore implements IEvaluate {
@@ -16,6 +18,16 @@ public class IEvaluateCore implements IEvaluate {
 
     private void init() {
 
+    }
+
+    @Override
+    public <T extends IEvaluatePlugin> T getPlugin(String name, Class<T> clazz) {
+        return null;
+    }
+
+    @Override
+    public Map<String, IEvaluatePlugin> getRegisteredPlugins() {
+        return null;
     }
 
     @Override

f5a0be1fb8a76b9dcba15a17bbf61ea694b10e6a
Andrew Wen <Wen.Andrew@mayo.edu>
2019-06-18 15:01:03 -0500

    Complete basic plugin management functionality

--- src/main/java/edu/mayo/dhs/ievaluate/core/plugins/PluginManagerCore.java
+++ src/main/java/edu/mayo/dhs/ievaluate/core/plugins/PluginManagerCore.java
@@ -1,5 +1,8 @@
 package edu.mayo.dhs.ievaluate.core.plugins;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import edu.mayo.dhs.ievaluate.api.IEvaluate;
 import edu.mayo.dhs.ievaluate.api.plugins.IEvaluatePlugin;
 import edu.mayo.dhs.ievaluate.api.plugins.PluginDescriptor;
@@ -7,9 +10,13 @@ import edu.mayo.dhs.ievaluate.api.plugins.PluginManager;
 import edu.mayo.dhs.ievaluate.core.plugins.classloader.PluginClassLoader;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
 
 /**
  * Heavily inspired by PL4J's implementation of plugin management, but modified to suit our use cases
@@ -17,60 +24,171 @@ import java.util.Objects;
 public class PluginManagerCore implements PluginManager {
     private Map<String, IEvaluatePlugin> registeredPlugins;
     private Map<String, PluginClassLoader> pluginClassLoaders;
-
+    private Map<String, PluginDescriptor> descriptors;
 
     public void loadPlugins(File pluginJarDir, File pluginConfigDir) {
-        Map<PluginDescriptor, File> descriptors = loadDescriptorsForFiles(pluginJarDir);
-        createClassLoaders(pluginJarDir);
+        loadDescriptorsAndClassloaders(pluginJarDir);
+        registerPlugins(pluginConfigDir);
     }
 
-    private Map<PluginDescriptor, File> loadDescriptorsForFiles(File pluginJarDir) {
-        for (File f : Objects.requireNonNull(pluginJarDir.listFiles())) {
-
-        }
-        return null;
-    }
-
-    private void createClassLoaders(File pluginsDir) {
+    private void loadDescriptorsAndClassloaders(File pluginsDir) {
         this.pluginClassLoaders = new HashMap<>();
-        // TODO
-
+        this.descriptors = new HashMap<>();
+        ObjectMapper om = new ObjectMapper();
+        for (File f : Objects.requireNonNull(pluginsDir.listFiles())) {
+            if (f.getName().endsWith(".jar")) { // Ignore non-jars
+                try (JarFile jar = new JarFile(f)) {
+                    ZipEntry entry = jar.getEntry("plugin.json");
+                    if (entry == null) {
+                        IEvaluate.getLogger().warn("Skipping plugin JAR with no plugin.json: " + f.getName());
+                        continue;
+                    }
+                    PluginDescriptor descriptor = om.readValue(jar.getInputStream(entry), PluginDescriptor.class);
+                    IEvaluate.getLogger().info("Loading plugin " + descriptor.getName());
+                    PluginClassLoader classLoader = new PluginClassLoader(this, descriptor, f, getClass().getClassLoader());
+                    pluginClassLoaders.put(descriptor.getName(), classLoader);
+                    descriptors.put(descriptor.getName(), descriptor);
+                    IEvaluate.getLogger().info("Successfully loaded plugin " + descriptor.getName());
+                } catch (IOException e) {
+                    IEvaluate.getLogger().warn("Failed to load plugin " + f.getName(), e);
+                }
+            }
+        }
     }
 
-
-    public void initializePlugins() {
-        registeredPlugins.forEach((name, plugin) -> {
-            IEvaluate.getLogger().info("Initializing plugin " + name);
+    private void registerPlugins(File confDir) {
+        this.registeredPlugins = new HashMap<>();
+        ObjectMapper om = new ObjectMapper();
+        descriptors.forEach((name, descriptor) -> {
+            // Copy and load configuration
+            File pluginConfDir = new File(confDir, name);
+            if (!pluginConfDir.isDirectory() || (!pluginConfDir.exists() && !pluginConfDir.mkdirs())) {
+                IEvaluate.getLogger().fatal("Could not create plugin configuration directory for " + name + "!");
+                System.exit(-1); // Fatal error
+            }
+            JsonNode currConfig;
+            File conf = new File(pluginConfDir, "config.json");
+            if (!conf.exists()) {
+                try (InputStream is = pluginClassLoaders.get(name).getResourceAsStream("/config.json")) {
+                    currConfig = om.readTree(is);
+                    om.writerWithDefaultPrettyPrinter().writeValue(conf, currConfig);
+                } catch (IOException e) {
+                    IEvaluate.getLogger().error("Could not copy plugin configuration file for " + name, e);
+                }
+            } else {
+                try (InputStream is = pluginClassLoaders.get(name).getResourceAsStream("/config.json")) {
+                    JsonNode defaultConfig = om.readTree(is);
+                    currConfig = om.readTree(conf);
+                    currConfig = mergeDefaultconfigIntoCurrent(defaultConfig, currConfig, name);
+                    om.writerWithDefaultPrettyPrinter().writeValue(conf, currConfig);
+                } catch (IOException e) {
+                    IEvaluate.getLogger().error("Could not copy plugin configuration file for " + name, e);
+                }
+            }
+            // Now try to load the plugin
+            String mainClass = descriptor.getMainClass();
             try {
-                plugin.onInit();
-                IEvaluate.getLogger().info("Successfully initialized plugin " + name);
-            } catch (Throwable t) {
-                IEvaluate.getLogger().fatal("Failure initializing " + name, t);
-                System.exit(-1); // Fatal fail to prevent security leak (from, e.g., an authentication plugin)
+                Class<?> pluginClazz = Class.forName(mainClass, true, pluginClassLoaders.get(name));
+                Constructor<?> ctor = pluginClazz.getConstructor();
+                registeredPlugins.put(name, (IEvaluatePlugin) ctor.newInstance());
+            } catch (ClassCastException | IllegalAccessException | InstantiationException | InvocationTargetException
+                    | ClassNotFoundException | NoSuchMethodException e) {
+                IEvaluate.getLogger().fatal("Could not load plugin " + name, e);
+                System.exit(-1);
             }
         });
+    }
+
+    public void initializePlugins() {
+        Set<String> visiting = new HashSet<>();
+        Set<String> initialized = new HashSet<>();
+        registeredPlugins.forEach((name, plugin) -> initializePluginRecursive(visiting, initialized, name, plugin));
+    }
+
+    private void initializePluginRecursive(Set<String> visiting, Set<String> initialized, String name, IEvaluatePlugin plugin) {
+        // Check and make sure this isn't already initialized successfully. If it is, we do not have to do anything further
+        if (initialized.contains(name)) {
+            return;
+        }
+        // Use visiting to prevent circular loading
+        if (visiting.contains(name)) {
+            IEvaluate.getLogger().fatal("Circular dependencies in plugin, dependency graph for " + name + " resulted in a cyclic load");
+            System.exit(-1); // Fatal exit
+        }
+        visiting.add(name);
+        for (String s : descriptors.get(name).getRequired()) {
+            initializePluginRecursive(visiting, initialized, s, registeredPlugins.get(s));
+        }
+        visiting.remove(name); // We are no longer recursing, thus no longer at risk of circular dependencies
+        IEvaluate.getLogger().info("Initializing plugin " + name);
+        try {
+            plugin.onInit();
+            IEvaluate.getLogger().info("Successfully initialized plugin " + name);
+        } catch (Throwable t) {
+            IEvaluate.getLogger().fatal("Failure initializing " + name, t);
+            System.exit(-1); // Fatal fail to prevent security leak (from, e.g., an authentication plugin)
+        }
+        initialized.add(name);
     }
 
     public void enablePlugins() {
-        registeredPlugins.forEach((name, plugin) -> {
-            IEvaluate.getLogger().info("Enabling plugin " + name);
-            try {
-                plugin.onEnable();
-                IEvaluate.getLogger().info("Successfully enabled plugin " + name);
-            } catch (Throwable t) {
-                IEvaluate.getLogger().fatal("Failure enabling " + name, t);
-                System.exit(-1); // Fatal fail to prevent security leak (from, e.g., an authentication plugin)
-            }
-        });
+        Set<String> visiting = new HashSet<>();
+        Set<String> initialized = new HashSet<>();
+        registeredPlugins.forEach((name, plugin) -> enablePluginRecursive(visiting, initialized, name, plugin));
     }
 
-    public PluginManagerCore() {
-        this.registeredPlugins = new HashMap<>();
+    private void enablePluginRecursive(Set<String> visiting, Set<String> enabled, String name, IEvaluatePlugin plugin) {
+        // Check and make sure this isn't already enabled successfully. If it is, we do not have to do anything further
+        if (enabled.contains(name)) {
+            return;
+        }
+        // Use visiting to prevent circular loading
+        if (visiting.contains(name)) {
+            IEvaluate.getLogger().fatal("Circular dependencies in plugin, dependency graph for " + name + " resulted in a cyclic load");
+            System.exit(-1); // Fatal exit
+        }
+        visiting.add(name);
+        for (String s : descriptors.get(name).getRequired()) {
+            enablePluginRecursive(visiting, enabled, s, registeredPlugins.get(s));
+        }
+        visiting.remove(name); // We are no longer recursing, thus no longer at risk of circular dependencies
+        IEvaluate.getLogger().info("Enabling plugin " + name);
+        try {
+            plugin.onEnable();
+            IEvaluate.getLogger().info("Successfully enabled plugin " + name);
+        } catch (Throwable t) {
+            IEvaluate.getLogger().fatal("Failure enabling " + name, t);
+            System.exit(-1); // Fatal fail to prevent security leak (from, e.g., an authentication plugin)
+        }
+        enabled.add(name);
+    }
+
+    private JsonNode mergeDefaultconfigIntoCurrent(JsonNode defaultConfig, JsonNode currConfig, String currPlugin) {
+        if (!(defaultConfig instanceof ObjectNode)) {
+            return currConfig; // This is a value, not a mapping, so we just return the default value
+        }
+        Map<String, JsonNode> toSet = new HashMap<>();
+        defaultConfig.fields().forEachRemaining(e -> {
+            if (currConfig.has(e.getKey())) {
+                JsonNode result = mergeDefaultconfigIntoCurrent(e.getValue(), currConfig.get(e.getKey()), currPlugin);
+                toSet.put(e.getKey(), result);
+            } else {
+                toSet.put(e.getKey(), e.getValue());
+            }
+        });
+        if (currConfig instanceof ObjectNode) {
+            toSet.forEach(((ObjectNode) currConfig)::set);
+        } else {
+            if (toSet.size() > 0) {
+                IEvaluate.getLogger().warn("[" + currPlugin + "] Default config has an object definition, whereas configuration value has a " + currConfig.getClass().getSimpleName());
+            }
+        }
+        return currConfig;
     }
 
     @Override
     public <T extends IEvaluatePlugin> T getPlugin(String name, Class<T> clazz) {
-        IEvaluatePlugin plugin = registeredPlugins.get(name.toUpperCase());
+        IEvaluatePlugin plugin = registeredPlugins.get(name);
         if (plugin == null) {
             return null;
         }
@@ -86,5 +204,13 @@ public class PluginManagerCore implements PluginManager {
     @Override
     public Map<String, IEvaluatePlugin> getRegisteredPlugins() {
         return registeredPlugins;
+    }
+
+    public Map<String, PluginClassLoader> getPluginClassLoaders() {
+        return pluginClassLoaders;
+    }
+
+    public Map<String, PluginDescriptor> getPluginDescriptors() {
+        return descriptors;
     }
 }
--- src/main/java/edu/mayo/dhs/ievaluate/core/plugins/classloader/PluginClassLoader.java
+++ src/main/java/edu/mayo/dhs/ievaluate/core/plugins/classloader/PluginClassLoader.java
@@ -1,11 +1,20 @@
 package edu.mayo.dhs.ievaluate.core.plugins.classloader;
 
+import edu.mayo.dhs.ievaluate.api.IEvaluate;
 import edu.mayo.dhs.ievaluate.api.plugins.PluginDescriptor;
 import edu.mayo.dhs.ievaluate.core.plugins.PluginManagerCore;
 
+import java.io.Closeable;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.JarURLConnection;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.WeakHashMap;
+import java.util.jar.JarFile;
 
 /**
  * Special class-loader for plugins that prioritizes children first so as to reduce dependency overwrite issues
@@ -15,9 +24,138 @@ public class PluginClassLoader extends URLClassLoader {
     private PluginManagerCore pluginManager;
     private PluginDescriptor descriptor;
 
-    public PluginClassLoader(PluginManagerCore pluginManager, File pluginFile, URL[] urls, ClassLoader parent) {
-        super(urls, parent);
+    public PluginClassLoader(PluginManagerCore pluginManager, PluginDescriptor descriptor, File pluginFile, ClassLoader parent) {
+        super(new URL[0], parent); // Start off with empty URL classpath (we will add plugin JAR) then delegate back ot parent
         this.pluginManager = pluginManager;
+        this.descriptor = descriptor;
+        this.addJar(pluginFile);
+    }
 
+    /* Mirrored from URLClassLoader
+     * A map (used as a set) to keep track of closeable local resources
+     * (either JarFiles or FileInputStreams). We don't care about
+     * Http resources since they don't need to be closed.
+     *
+     * If the resource is coming from a jar file
+     * we keep a (weak) reference to the JarFile object which can
+     * be closed if URLClassLoader.close() called. Due to jar file
+     * caching there will typically be only one JarFile object
+     * per underlying jar file.
+     *
+     * For file resources, which is probably a less common situation
+     * we have to keep a weak reference to each stream.
+     */
+
+    private WeakHashMap<Closeable,Void>
+            openFiles = new WeakHashMap<>();
+
+    @Override
+    public void addURL(URL url) { // Public access to protected method
+        super.addURL(url);
+    }
+
+    private void addJar(File jar) {
+        try {
+            addURL(jar.getCanonicalFile().toURI().toURL()); // Use canonical for consistency
+        } catch (IOException e) {
+            IEvaluate.getLogger().error(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public Class<?> loadClass(String className) throws ClassNotFoundException {
+        synchronized (getClassLoadingLock(className)) {
+            if (className.startsWith("java.")) {
+                return findSystemClass(className); // Shortcut for system classes
+            }
+
+            // Check if already loaded
+            Class<?> loadedClass = findLoadedClass(className);
+            if (loadedClass != null) {
+                return loadedClass;
+            }
+
+            // Not loaded, so first try to find in direct plugin
+            try {
+                loadedClass = findClass(className);
+                return loadedClass;
+            } catch (ClassNotFoundException e) {
+                // try next step
+            }
+
+            // Look in Dependencies
+            loadedClass = loadDependencyClass(className);
+            if (loadedClass != null) {
+                return loadedClass;
+            }
+
+            // Not found within plugin (or dependency) classes, return control to standard classloader
+            return super.loadClass(className);
+
+        }
+    }
+
+    private Class<?> loadDependencyClass(String className) {
+        List<String> dependencies = descriptor.getRequired();
+        for (String dependency : dependencies) {
+            ClassLoader classLoader = pluginManager.getPluginClassLoaders().get(dependency.toLowerCase());
+
+            // If not found, well there is an issue but let's not die here
+            if (classLoader == null) {
+                IEvaluate.getLogger().error(String.format("%s has dependency on %s but no corresponding classloader was found", descriptor.getName(), dependency));
+                continue;
+            }
+
+            try {
+                return classLoader.loadClass(className);
+            } catch (ClassNotFoundException ignored) {
+                // Cannot prevent class not found exception, so we catch it here silently and move on to the next
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public URL getResource(String name) {
+        // Check locally
+        URL url = findResource(name);
+        if (url != null) {
+            return url;
+        }
+        // Don't check dependencies as they will also define (as an example)
+        // Fallback to default behaviour
+        return super.getResource(name);
+    }
+
+    @Override
+    public InputStream getResourceAsStream(String name) {
+        // Check locally
+        URL url = findResource(name);
+        if (url != null) {
+            try {
+                URLConnection urlc = url.openConnection();
+                InputStream is = urlc.getInputStream();
+                if (urlc instanceof JarURLConnection) {
+                    JarURLConnection juc = (JarURLConnection) urlc;
+                    JarFile jar = juc.getJarFile();
+                    synchronized (openFiles) {
+                        if (!openFiles.containsKey(jar)) {
+                            openFiles.put(jar, null);
+                        }
+                    }
+                } else if (urlc instanceof sun.net.www.protocol.file.FileURLConnection) {
+                    synchronized (openFiles) {
+                        openFiles.put(is, null);
+                    }
+                }
+                return is;
+            } catch (IOException ignored) {
+                // Usually, this would be return null, but we fall back on parent instead
+            }
+        }
+        // Don't check dependencies as they will also define (as an example)
+        // Fallback to default behaviour
+        return super.getResourceAsStream(name);
     }
 }

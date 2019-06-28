24e345f1bec5b0d5c8d293bbe5cdde212241f6eb
Andrew Wen <Wen.Andrew@mayo.edu>
2019-06-28 17:03:45 -0500

    Additional work on clinical application implementation

--- src/main/java/edu/mayo/dhs/ievaluate/core/jobs/RESTfulApplicationJob.java
+++ src/main/java/edu/mayo/dhs/ievaluate/core/jobs/RESTfulApplicationJob.java
@@ -3,7 +3,15 @@ package edu.mayo.dhs.ievaluate.core.jobs;
 import edu.mayo.dhs.ievaluate.api.models.jobs.ApplicationJob;
 
 /**
- * A default implementation of an application job that utilizes RESTful endpoints for execution and state updates
+ * A default implementation of an application job that utilizes RESTful endpoints for execution and state updates.
+ *
+ * All REST endpoints must contain the macro %JOB_UID% which will be substituted with the appropriate {@link #jobUID}
  */
-public class RESTfulApplicationJob extends ApplicationJob {
+public abstract class RESTfulApplicationJob extends ApplicationJob {
+
+    private String jobStatusRestEndpoint;
+
+    public RESTfulApplicationJob(String jobStatusRestEndpoint) {
+        this.jobStatusRestEndpoint = jobStatusRestEndpoint;
+    }
 }
